6eca4e60896d7125f63403ad3b93162068d41117
geanly7 <geanly7@gmail.com>
2026-09-01 01:16:13 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker profesional con auto-actualización y soporte offline para Familia GFB & Sinergia
-const CACHE_NAME = 'familia-gfb-v15.6';
+const CACHE_NAME = 'familia-gfb-v15.7';
 const ASSETS = [
   './',
   './index.html',
